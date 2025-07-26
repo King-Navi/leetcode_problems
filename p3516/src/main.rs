e138e3c517eb8f8b00e4909e1c264537481b50c2
King-Navi <168604098+King-Navi@users.noreply.github.com>
2025-07-26 08:48:22 -0600

    p3516 solved

---
+++ p3516/src/main.rs
@@ -0,0 +1,30 @@
+pub struct Solution;
+fn main() {
+    let s  =Solution::find_closest(2, 7, 4);
+    println!("{s}");
+}
+
+impl Solution {
+    pub fn find_closest(x: i32, y: i32, z: i32) -> i32 {
+        let x = steps_requiered(&x, &z);
+        let y = steps_requiered(&y, &z);
+        if x == y {
+            return 0;
+        }else if x > y {
+            return 2;
+        }else {
+            return 1;
+        }
+    }
+}
+
+pub fn steps_requiered(n : & i32 , z : & i32) -> i32{
+    if n == z {
+        return 0;
+    }else if n > z {
+        return n- z;
+    }else {
+        return z-n;
+    }
+
+}
