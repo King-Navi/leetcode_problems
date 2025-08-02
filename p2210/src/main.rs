f6843edecfe72694be03ba65bc136a4ae07b915c
King-Navi <168604098+King-Navi@users.noreply.github.com>
2025-08-02 10:37:28 -0600

    p2210 solved

--- p2210/src/main.rs
+++ p2210/src/main.rs
@@ -1,36 +1,124 @@
-pub struct Solution; 
+pub struct Solution;
 fn main() {
-    println!("Hello, world!");
+    Solution::count_hill_valley(vec![2, 4, 1, 1, 6, 5]);
 }
 
-//
-//
-//
-//
 impl Solution {
     pub fn count_hill_valley(nums: Vec<i32>) -> i32 {
-        let last_index: usize = nums.iter().count(); 
-        for (i , n) in nums.iter().enumerate() {
-            if i == 0  {
+        let mut counter_result = 0;
+
+        let last_index: usize = nums.iter().count() - 1;
+
+        for (i, &n) in nums.iter().enumerate() {
+            if i == 0 {
+                //first number never a hill or valley
                 continue;
             }
-            if i == nums.iter().count() - 1 {
-                break;
-            }
-            //hill
-            if nums[i-1] < {
-                
+            if nums[i] == nums[i -1] {
+                //same number no sense to check
+                continue;
             }
 
+            if i == last_index {
+                //last number never a hill or valley
+                break;
+            }
+
+            //Only if isn't outboundindex
+            {
+                //hill
+                let mut behind_number: Option<i32> = None;
+                let mut front_number: Option<i32> = None;
+                {
+                    let mut index_ = i +1;
+                    while true {
+                        if nums[index_] < n {
+                            front_number = Some(nums[index_]);
+                            break;
+                        }
+                        if nums[index_] > n  {
+                            break;
+                        }
+                        if index_ >= last_index {
+                            break;
+                        }
+                        index_ +=1;
+                    }
+                }
+                {
+                    let mut index_ = i - 1;
+                    while true {
+                        if nums[index_] < n {
+                            behind_number = Some(nums[index_]);
+                            break;
+                        }
+                        if nums[index_] > n  {
+                            break;
+                        }
+                        if index_ <= 0 {
+                            break;
+                        }
+                        index_ -=1;
+                    }
+                }
+
+                //check if is hill
+                if behind_number.is_some() && front_number.is_some() {
+                    counter_result +=1;
+                    continue;
+                }
+            }
+
+            {
+                //valley
+                let mut behind_number: Option<i32> = None;
+                let mut front_number: Option<i32> = None;
+                {
+                    let mut index_ = i +1;
+                    while true {
+                        if nums[index_] > n {
+                            front_number = Some(nums[index_]);
+                            break;
+                        }
+                        if nums[index_] < n  {
+                            break;
+                        }
+                        if index_ >= last_index {
+                            break;
+                        }
+                        index_ +=1;
+                    }
+                }
+                {
+                    let mut index_ = i - 1;
+                    while true {
+                        if nums[index_] > n {
+                            behind_number = Some(nums[index_]);
+                            break;
+                        }
+                        if nums[index_] < n  {
+                            break;
+                        }
+                        if index_ <= 0 {
+                            break;
+                        }
+                        index_ -=1;
+                    }
+                }
+
+                //check if is valley
+                if behind_number.is_some() && front_number.is_some() {
+                    counter_result +=1;
+                    continue;
+                }
+            }
         }
-        0
+        println!("{:?}", counter_result);
+        counter_result
     }
 }
 
-pub fn check_next_hill() -> (i32, i32){
+// //return de index and the
+// pub fn check_next_hill_posible_number() -> (i32, i32) {}
 
-}
-
-pub fn check_next_valley() -> (i32, i32){
-    
-}
+// pub fn check_next_hill_posible_number() -> (i32, i32) {}
