f846a2a6498fb6bee5bcd218bd156ab4da34f788
King-Navi <168604098+King-Navi@users.noreply.github.com>
2025-08-07 09:52:35 -0600

    p3498 solved

---
+++ p3498/src/main.rs
@@ -0,0 +1,18 @@
+struct  Solution;
+fn main() {
+    Solution::reverse_degree("abc".to_string());
+}
+
+
+impl Solution {
+    pub fn reverse_degree(s: String) -> i32 {
+        let mut sum = 0;
+        for (i , c) in s.chars().enumerate() {
+            let num = (c as i32 - 123).abs();
+            sum += num * (i as i32 + 1) ;
+
+        }
+        println!("{:?}", sum);
+        sum 
+    }
+}
