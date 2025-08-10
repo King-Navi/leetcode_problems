afeb879c213b8dd032b2b9c0c29cf6335a630679
King-Navi <168604098+King-Navi@users.noreply.github.com>
2025-08-09 20:19:11 -0600

    p3487 solved

---
+++ p3487/src/main.rs
@@ -0,0 +1,33 @@
+
+struct Solution;
+fn main() {
+    Solution::max_sum(vec![1,2,-1,-2,1,0,-1]);
+}
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn max_sum(nums: Vec<i32>) -> i32 {
+        let mut hash_set: HashSet<i32> = HashSet::new();
+        let mut  v = vec![];
+        let mut sum = 0;
+        let mut sum_negative = i32::MIN;
+        for n in nums {
+            if n <= 0 {
+                if sum_negative < n {
+                    sum_negative = n;
+                }
+                continue;
+            }
+            if !hash_set.contains(&n) {
+                hash_set.insert(n);
+                v.push(n);
+                sum += n;
+            }
+        }
+        println!("{:?}", sum);
+        if sum == 0 {
+            return sum_negative;
+        }
+        sum
+    }
+}
