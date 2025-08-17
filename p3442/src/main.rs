e7a5a3b978b7dfd96ad0a1ec112964c538bd2e11
King-Navi <168604098+King-Navi@users.noreply.github.com>
2025-08-17 08:36:29 -0600

    p3442 solved

---
+++ p3442/src/main.rs
@@ -0,0 +1,21 @@
+
+struct  Solution;
+fn main() {
+    let s  =Solution::max_difference("mmsmsym".to_string());
+    println!("{s}");
+}
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn max_difference(s: String) -> i32 {
+        let mut hashmap:HashMap<char, i32> = HashMap::new();
+        for c in s.chars() {
+           *hashmap.entry(c).or_insert(0) +=1; 
+        }
+        let odd_frec =hashmap.iter().filter(|&(i, x)| x % 2 != 0).max_by_key(|&(i, c)| c).unwrap();
+        let even_frec =hashmap.iter().filter(|&(i, x)| x % 2 == 0).min_by_key(|&(i, c)|c).unwrap();
+        let result =odd_frec.1 - even_frec.1;
+        result
+    }
+}
+
