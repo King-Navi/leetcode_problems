a5977e58ea7d1c82cd4ef7db71cc99e293598358
King-Navi <168604098+King-Navi@users.noreply.github.com>
2025-08-18 23:24:35 -0600

    Add

--- p3442/src/main.rs
+++ p3442/src/main.rs
@@ -12,8 +12,8 @@ impl Solution {
         for c in s.chars() {
            *hashmap.entry(c).or_insert(0) +=1; 
         }
-        let odd_frec =hashmap.iter().filter(|&(i, x)| x % 2 != 0).max_by_key(|&(i, c)| c).unwrap();
-        let even_frec =hashmap.iter().filter(|&(i, x)| x % 2 == 0).min_by_key(|&(i, c)|c).unwrap();
+        let odd_frec =hashmap.iter().filter(|&(_, x)| x % 2 != 0).max_by_key(|&(_, c)| c).unwrap();
+        let even_frec =hashmap.iter().filter(|&(_, x)| x % 2 == 0).min_by_key(|&(_, c)|c).unwrap();
         let result =odd_frec.1 - even_frec.1;
         result
     }
