2a34c9bd6a4654fad5821d72dd921c318ca23436
King-Navi <168604098+King-Navi@users.noreply.github.com>
2025-08-20 23:39:47 -0600

    3427 solved

---
+++ p3427/src/main.rs
@@ -0,0 +1,22 @@
+struct Solution;
+fn main() {
+    Solution::subarray_sum(vec![3,2,1]);
+}
+
+impl Solution {
+    pub fn subarray_sum(nums: Vec<i32>) -> i32 {
+        let mut result = 0;
+        for (i , n) in nums.iter().enumerate() {
+            let mut start = i  as i32 - nums[i];
+            if start < 0 {
+                start = 0;
+            }
+            let stop = i + 1; 
+            let slice = &nums[start as usize.. stop];
+            for s in slice  {
+                result += s;
+            }
+        }
+        result      
+    }
+}
