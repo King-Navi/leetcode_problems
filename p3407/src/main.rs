34d2ce2100d3e1e4baaae2584c8d3f6b285993c4
King-Navi <168604098+King-Navi@users.noreply.github.com>
2025-08-31 19:13:26 -0600

    p3407 solved

--- p3407/src/main.rs
+++ p3407/src/main.rs
@@ -10,55 +10,25 @@ fn main() {
     println!("{:?}", s); //true
     let s = Solution::has_match("mlmww".to_string(), "ml*wl".to_string());
     println!("{:?}", s); //false
+    let s = Solution::has_match("tokk".to_string(), "t*t".to_string());
+    println!("{:?}", s); //false
+    let s = Solution::has_match("wwmwww".to_string(), "wm*".to_string());
+    println!("{:?}", s); //true
+    let s = Solution::has_match("ggmgmrmrm".to_string(), "*gmg".to_string());
+    println!("{:?}", s); //true
+    let s = Solution::has_match("mqmmmqmqbmqq".to_string(), "mmqm*b".to_string());
+    println!("{:?}", s); //true
+
+    // let s: Vec<char> = "leetcode".chars().collect();
+    // let ss: Vec<char> = "ee".chars().collect();
+    // let res = has_substring(&s,& ss);
+    // println!("{:?}", res);
 }
 impl Solution {
-    // pub fn has_match(s: String, p: String) -> bool {
-    //     let mut result = false;
-    //     let s_vec: Vec<char> = s.chars().collect();
-    //     let p_vec: Vec<char> = p.chars().collect();
-    //     let mut index_of_p = 0;
-    //     let mut max_index_p = p_vec.len() - 1;
-    //     let mut index_second_part_p = 0;
-    //     let mut second_part_p = false;
-    //     if p_vec.len() == 1 && '*' == p_vec[0]  {
-    //         return true;
-    //     }
-    //     for (i, &c) in s_vec.iter().enumerate() {
-
-    //         if index_of_p <= max_index_p && '*' == p_vec[index_of_p] {
-    //             index_of_p += 1;
-    //             index_second_part_p = index_of_p;
-    //             second_part_p = true;
-    //         }
-
-    //         if index_of_p <= max_index_p
-    //             && c == p_vec[index_of_p] {
-
-    //         } else {
-    //             if second_part_p {
-    //                 index_of_p = index_second_part_p;
-    //                 continue;
-    //             } else {
-    //                 index_of_p = 0;
-    //                 continue;
-    //             }
-    //         }
-
-    //         if index_of_p >= max_index_p{
-    //             result = true;
-    //             break;
-    //         }
-    //         index_of_p += 1;
-    //         if s_vec.len() -1 == i {
-    //             return true;
-    //         }
-
-    //     }
-
-    //     result
-    // }
     pub fn has_match(s: String, p: String) -> bool {
-        let mut result = false;
+        if p.len() == 1 && p.chars().collect::<Vec<char>>()[0] == '*' {
+            return true;
+        }
         let mut first_part: Vec<char> = vec![];
         let mut second_part: Vec<char> = vec![];
         {
@@ -66,6 +36,7 @@ impl Solution {
             for c in p.chars() {
                 if c == '*' {
                     flag_second_part = true;
+                    continue;
                 }
                 if !flag_second_part {
                     first_part.push(c);
@@ -75,14 +46,134 @@ impl Solution {
                 }
             }
         }
+        let s_chars : Vec<char> = s.chars().collect();
+        let index_second_part = has_substring(&s_chars, &first_part);
+        print!("{:?} , ", index_second_part);
+        if index_second_part < 0 {
+            return false;
+        }
+        let last_result = has_substring(&s_chars[index_second_part as usize .. ], &second_part);
+        println!("{:?}", last_result);
+        if last_result < 0 {
+            return false;
+        }
+        true
+    }
+}
+
+
+
+pub fn has_substring(main: &[char], substring : &[char] ) -> isize {
+    if substring.is_empty() {
+        return 0;
+    }
+    for (i, _) in main.iter().enumerate() {
+        let mut index_copy = i;
+        let mut match_result = true;
+        let mut saved_j = 0;
+        for j in 0 .. substring.len() {
+            if index_copy >= main.len() {
+                match_result = false;
+                break;
+            }
+            if  substring[j] != main[index_copy] {
+                match_result = false;
+                break;
+            } 
+            index_copy += 1;
+            saved_j = j + i;
+        }
+        if match_result {
+            //Si hay coincidencia
+            return saved_j as isize + 1;
+        }
+    }
+    -1
+}
+
+
+/*
+pub fn has_match(s: String, p: String) -> bool {
+        if p.len() == 1 && p.chars().collect::<Vec<char>>()[0] == '*' {
+            return true;
+        }
+        let mut first_part: Vec<char> = vec![];
+        let mut second_part: Vec<char> = vec![];
         {
             let mut flag_second_part = false;
-            let index = 0;
-            for c in s.chars() {
+            for c in p.chars() {
+                if c == '*' {
+                    flag_second_part = true;
+                    continue;
+                }
+                if !flag_second_part {
+                    first_part.push(c);
+                }
+                if flag_second_part {
+                    second_part.push(c);
+                }
+            }
+        }
+        let s_chars : Vec<char> = s.chars().collect();
+        let mut index_s_save = 0;
+        'first: {
+            if first_part.is_empty() {
+                break 'first;
+            }
+            let mut index_first_part: isize = -1;
+            for (i , &c) in s_chars.iter().enumerate() {
+                index_s_save = i;
+                index_first_part +=1;
+                if  index_first_part >= 0
+                    && c == first_part[index_first_part as usize] 
+                    && (index_first_part as usize) < first_part.len() {
+                    //continue;
+                    if index_first_part == first_part.len() as isize - 1 {
+                        index_s_save +=1;
+                        break;
+                    }
+                } else {
+                    if c == first_part[0] {
+                        index_first_part = 0;
+                    }else {
+                       index_first_part = -1;  
+                    }
+                    
+                }
+            }
+            if index_first_part != first_part.len() as isize - 1 {
+                return false;
+            }
+        }
+        'second : {
+            if second_part.is_empty() {
+                break 'second;
+            }
+            let mut index_second_part: isize = -1;
+            for i in index_s_save..s_chars.len() {
                 
+                index_second_part +=1;
+                if  index_second_part >= 0
+                    && s_chars[i] == second_part[index_second_part as usize] 
+                    && (index_second_part as usize) < second_part.len() {
+                    //continue;
+                    if index_second_part == second_part.len() as isize - 1 {
+                        break;
+                    }
+                } else {
+                    if s_chars[i] == second_part[0] {
+                        index_second_part = 0;
+                    } else {
+                      index_second_part = -1;   
+                    }
+                }
+            }
+            if index_second_part != second_part.len() as isize - 1 {
+                return  false;
             }
         }
 
-        result
+        true
     }
-}
+
+*/
