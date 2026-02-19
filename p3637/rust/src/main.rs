3a29680bd634a414b53912490afc150fc262f62b
King-navi <168604098+King-Navi@users.noreply.github.com>
2026-02-19 07:44:13 -0600

    p3637 solved

--- p3637/rust/src/main.rs
+++ p3637/rust/src/main.rs
@@ -2,32 +2,26 @@
 struct Solution;
 impl Solution {
     pub fn is_trionic(nums: Vec<i32>) -> bool {
-        let mut last_number: &i32 = &nums[0];
-        let max_index =nums.len() -1;
-        let mut last_index =nums.len() -1;
-        for i in 1..=max_index {
-            if last_number < &nums[i] {
-                last_number = &nums[i];
-            }else {
-                last_number = &nums[i];
-                last_index = i;
-                break;
-            }
+        let n = nums.len();
+        let mut i = 1;
+        while i < n && nums[i] > nums[i - 1] {
+            i += 1;
+        }    
+        if i == 1 || i == n {
             return false;
         }
 
-        for i in last_index..=max_index {
-            if last_number > &nums[i] {
-                last_number = &nums[i];
-            }else {
-                last_index = i;
-                break;
-                
-            }
-            
+        while i < n && nums[i] < nums[i - 1] {
+            i += 1;
+        }    
+        if i == 1 || i == n {
             return false;
         }
-        false
+
+        while i < n && nums[i] > nums[i - 1] {
+            i += 1;
+        }    
+        i == n  
     }
 }
 
