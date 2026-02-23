42c2f4cb0199fcbba926b672e1bc0686d701d006
King-navi <168604098+King-Navi@users.noreply.github.com>
2026-02-23 06:44:43 -0600

    p3674 solved

---
+++ p3674/rust/src/main.rs
@@ -0,0 +1,18 @@
+use std::collections::HashMap;
+
+struct Solution;
+impl Solution {
+    pub fn min_operations(nums: Vec<i32>) -> i32 {
+        let mut hashmap : HashMap<i32 , i32> = HashMap::new();
+        for n in nums {
+            *hashmap.entry(n).or_insert(0) += 1;
+        }
+        if hashmap.len() == 1 {
+            return 0;
+        }
+        1
+    }
+}
+fn main() {
+    println!("{}", Solution::min_operations(vec![1,2,5,5]));
+}
