508fb1bb6effd2593f3a73d0ab83f45b17e85488
King-navi <168604098+King-Navi@users.noreply.github.com>
2026-03-10 02:52:48 -0600

    p3379 solved

--- p3379/rust/src/main.rs
+++ p3379/rust/src/main.rs
@@ -1,3 +1,5 @@
+use std::ops::BitOrAssign;
+
 struct Solution;
 impl Solution {
     pub fn construct_transformed_array(nums: Vec<i32>) -> Vec<i32> {
@@ -9,14 +11,23 @@ impl Solution {
                 let a: i32 = i as i32 + position;
                 let new_pos = a % (lenght as i32);
                 result.push(nums[new_pos as usize]);
-                println!(" {} new position {}", a , new_pos);
+                println!("{}",nums[i]);
+                println!("{:?}",result);
             }else if nums[i] < 0 {
-                let a: i32 = i as i32 - position;
-                let new_pos = a.abs() % (lenght as i32);
+                let mut a: i32 = i as i32 + position as i32;
+                if a < 0 {
+                    a = lenght as i32 + a ;
+                }
+                a = a.abs();
+                println!("res {}",a);
+                //let new_pos = (i as i32 + nums[i]).rem_euclid(lenght as i32);
+                let new_pos = ((a % lenght) + length) % length;
+                println!("mod {}",new_pos);
                 result.push(nums[new_pos as usize]); 
-                println!("{} {}", a , new_pos);
+                println!("{}",nums[i]);
+                println!("{:?}",result);
             }else {
-                result[i] = nums[i];
+                result.push(nums[i]);
             }
         }
         result
@@ -24,6 +35,8 @@ impl Solution {
 
 }
 fn main() {
-    println!("{:?}", Solution::construct_transformed_array(vec![3,-2,1,1]));
-    println!("{:?}", Solution::construct_transformed_array(vec![-1,4,-1]));
+    //println!("{:?}", Solution::construct_transformed_array(vec![3,-2,1,1]));
+    //println!("{:?}", Solution::construct_transformed_array(vec![-1,4,-1]));
+    //println!("{:?}", Solution::construct_transformed_array(vec![-9,0]));
+    println!("{:?}", Solution::construct_transformed_array(vec![3,-2,1,-100]));
 }
