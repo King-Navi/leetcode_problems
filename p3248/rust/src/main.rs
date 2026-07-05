b56879e4e8875510739eafddf9cca0a233f9969c
King-navi <168604098+King-Navi@users.noreply.github.com>
2026-07-05 10:40:31 -0600

    more problems

---
+++ p3248/rust/src/main.rs
@@ -0,0 +1,63 @@
+struct Solution;
+impl Solution {
+    pub fn final_position_of_snake(n: i32, commands: Vec<String>) -> i32 {
+        let mut result = 0;
+        for command in commands {
+            result += process_comand(command, n);
+        }
+        result
+    }
+}
+
+fn process_comand(s:String, n:i32)->i32{
+    match s.as_str() {
+        "DOWN"=> n,
+        "UP"=> -n,
+        "RIGHT"=> 1,
+        "LEFT"=> -1,
+        _ => 0
+    }
+}  
+
+fn main() {
+    println!("Hello, world!");
+}
+
+
+/*
+There is a snake in an n x n matrix grid and can move in four possible directions. Each cell in the grid is identified by the position: grid[i][j] = (i * n) + j.
+
+The snake starts at cell 0 and follows a sequence of commands.
+
+You are given an integer n representing the size of the grid and an array of strings commands where each command[i] is either "UP", "RIGHT", "DOWN", and "LEFT". It's guaranteed that the snake will remain within the grid boundaries throughout its movement.
+
+Return the position of the final cell where the snake ends up after executing commands.
+
+ 
+
+Example 1:
+
+Input: n = 2, commands = ["RIGHT","DOWN"]
+
+Output: 3
+
+Explanation:
+
+0	1
+2	3
+
+Example 2:
+
+Input: n = 3, commands = ["DOWN","RIGHT","UP"]
+
+Output: 1
+
+Explanation:
+
+0	1	2
+3	4	5
+6	7	8
+
+
+
+ */
